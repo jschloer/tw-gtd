9d3135de08342b9a7bd8f8369b297d881f71f0e0
Jason Schloer <jschloer@hotmail.com>
2017-12-01 17:21:28 -0500

    Trying out vorpal as our method of invocation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 var inquirer = require('inquirer');
+var vorpal = require('vorpal')();
+
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
@@ -15,16 +17,53 @@ async function get_in_tasks() {
   return taskList;
   //console.log('taskList[0].description: ', taskList[0].description);
 }
-get_in_tasks().then(res => {
-  console.log('output: ', res);
-  inquirer
-    .prompt([
+const test = () => {
+  get_in_tasks().then(res => {
+    console.log('output: ', res);
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'task',
+          choices: res.map(task => ({
+            name: task.description,
+            value: task.id,
+          })),
+          message: 'Choose a task',
+        },
+      ])
+      .then(answers => {});
+  });
+};
+vorpal
+  .mode('in', 'Processes the inbox tasks')
+  .init(function(args, callback) {
+    this.log('Entering Inbox processing mode.');
+    get_in_tasks().then(tasks => {
+      this.log('The following tasks are in the inbox:');
+
+      tasks.forEach(task => {
+        this.log(`${task.id} ${task.description}`);
+      });
+      callback();
+    });
+  })
+  .action(function(command, cb) {
+    this.prompt([
       {
         type: 'list',
-        name: 'task',
-        choices: res.map(task => ({ name: task.description, value: task.id })),
-        message: 'Choose a task',
+        name: 'taskAction',
+        choices: [
+          { name: 'Make project', value: 'project' },
+          { name: 'Activate', value: 'activate' },
+        ],
+        message: 'What would you like to do with this item?',
       },
-    ])
-    .then(answers => {});
-});
+    ]).then(answers => {
+      const options = { sessionId: this.session.id };
+      vorpal._exitMode(options);
+      cb();
+    });
+  });
+
+vorpal.show();
