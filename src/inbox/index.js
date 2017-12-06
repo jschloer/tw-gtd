5b310f7d4553aa8691b1d319fc06a4c51ccb8168
Jason Schloer <jschloer@hotmail.com>
2017-12-06 16:01:59 -0500

    Seems to be able to handle basic project creation...

--- src/inbox/index.js
+++ src/inbox/index.js
@@ -3,6 +3,14 @@ var inquirer = require('inquirer');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
+function forEachPromise(items, fn, context) {
+  return items.reduce(function(promise, item) {
+    return promise.then(function() {
+      return fn(item, context);
+    });
+  }, Promise.resolve());
+}
+
 async function get_in_tasks() {
   const { stdout, stderr } = await exec('task status:pending +in export');
   const taskList = JSON.parse(stdout);
@@ -21,6 +29,15 @@ async function maybeLaterTask(taskId) {
   //console.log('stdout: ', stdout);
   return stdout;
   //return exec(`task ${taskId} mod -in +later`);
+}
+async function completeTask(taskId) {
+  const { stdout, stderr } = await exec(`task ${taskId} done`);
+  return stdout;
+}
+async function createTasks(tasks) {
+  return forEachPromise(tasks, newTask => {
+    return exec(`task add ${newTask}`);
+  });
 }
 const handleInboxTask = (task, vorpalInstance) => {
   //given a task, display it and give the user the first question
@@ -80,16 +97,100 @@ const handleNonActionableTask = (task, vorpalInstance) => {
     });
 };
 const handleActionableTask = (task, vorpalInstance) => {
-  console.log('actionable');
-};
-
-function forEachPromise(items, fn, context) {
-  return items.reduce(function(promise, item) {
-    return promise.then(function() {
-      return fn(item, context);
+  return vorpalInstance
+    .prompt([
+      {
+        type: 'confirm',
+        name: 'taskProject',
+        message: 'Does this action require more than one action?',
+      },
+    ])
+    .then(result => {
+      if (result.taskProject) {
+        return handleProjectTask(task, vorpalInstance);
+      } else {
+        return handleNonProjectTask(task, vorpalInstance);
+      }
     });
-  }, Promise.resolve());
-}
+};
+const handleProjectTask = (task, vorpalInstance) => {
+  return vorpalInstance
+    .prompt([
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the name of the project for this task? ',
+      },
+    ])
+    .then(result => {
+      // now ask the user to enter the names of tasks for this project
+      // how do we deal with contexts.... Let's just enter them with the task for now
+      //return vorpalInstance.prompt([{type: 'input', name:'task name'}])
+      //console.log('vorpalInstance: ', vorpalInstance);
+      return handleProjectAddTask(vorpalInstance, []).then(newTasks => {
+        const taskNames = newTasks.map(newTask => {
+          return `proj:${result.projectName} ${newTask}`;
+        });
+        console.log('taskNames: ', taskNames);
+        return createTasks(taskNames).then(() => {
+          return deleteTask(task.id);
+          //console.log('Now we would delete the original task');
+        });
+      });
+    });
+};
+const handleProjectAddTask = (vorpalInstance, newTasks) => {
+  // ask the user for the name of a task and add if they enter nothing, then end
+  return vorpalInstance
+    .prompt([
+      {
+        type: 'input',
+        name: 'taskName',
+        message: 'Enter a task name(leave blank if finished):',
+      },
+    ])
+    .then(result => {
+      if (result.taskName.trim() === '') {
+        return newTasks;
+      } else {
+        return handleProjectAddTask(vorpalInstance, [
+          ...newTasks,
+          result.taskName,
+        ]);
+      }
+    });
+};
+const handleNonProjectTask = (task, vorpalInstance) => {
+  return vorpalInstance
+    .prompt([
+      {
+        type: 'confirm',
+        name: 'doItNow',
+        message: 'Will this take <2 minutes?',
+      },
+    ])
+    .then(result => {
+      if (result.doItNow) {
+        return vorpalInstance
+          .prompt([
+            {
+              type: 'list',
+              name: 'complete',
+              message: 'You should do it now. Skip or mark as completed?',
+              choices: [
+                { name: 'Skip', value: false },
+                { name: 'Complete', value: true },
+              ],
+            },
+          ])
+          .then(secondResult => {
+            if (secondResult.complete) {
+              completeTask(task.id);
+            }
+          });
+      }
+    });
+};
 
 exports.default = vorpal => {
   vorpal
@@ -98,7 +199,11 @@ exports.default = vorpal => {
     .alias('in')
     .action(function(args, cb) {
       get_in_tasks().then(tasks => {
-        this.log('The following tasks are in the inbox:');
+        if (tasks.length > 0) {
+          this.log("Here's the first task in your inbox");
+        } else {
+          this.log('Inbox is empty');
+        }
         forEachPromise(tasks, handleInboxTask, this).then(() => {
           console.log('done');
           cb();
